9c2162c6cd6f8612f8a9ba17ed71f35027a521db
MariusAD <mariusadicu@gmail.com>
2022-02-25 10:43:16 -0600

    start

--- src/test/java/com/nextbasecrm/tests/US_5.java
+++ src/test/java/com/nextbasecrm/tests/US_5.java
@@ -1,10 +1,50 @@
 package com.nextbasecrm.tests;
 
+import com.nextbasecrm.utilities.CRM_Utilities;
+import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
 public class US_5 {
 
-    //1st step
+    public WebDriver driver;
 
-    //2nd step
+    @BeforeMethod
+    public void setupMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
 
+    @Test
+    public void test(){
+
+        driver.get("https://login2.nextbasecrm.com/");
+
+        CRM_Utilities.crm_login(driver, "helpdesk40@cybertekschool.com", "UserUser");
+
+
+        String myMsg ="test";
+        //driver.findElement(By.xpath("//iframe[@class='bx-editor-iframe']")).sendKeys(myMsg);//
+
+        //you mustr find send button locator and press on it
+
+        //you musr assert that msg got posted
+
+        //find element and use getText method and place in a string
+
+        //Assert.assertEquals(myMsg.equalsIgnoreCase());
+        //
+
+        //Assert
+
+
+
+    }
 
 }
