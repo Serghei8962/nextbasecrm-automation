a904c112f6d5703d378c9b62a222bb6b9c6b10f1
IrinaOrli <93168720+IrinaOrli@users.noreply.github.com>
2022-03-01 23:13:41 -0500

    US_10 test cases automation

--- src/test/java/com/nextbasecrm/tests/TaskCreation.java
+++ src/test/java/com/nextbasecrm/tests/TaskCreation.java
@@ -33,11 +33,11 @@ public class TaskCreation {
 
 
     @Test
-    public void task_creation_testHR() {
+    public void task_creation_test() {
 
 //As an HR user go to  the main page of the website
 
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.hr"), ConfigurationReader.getProperty("password"));
+        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
 
 //I'm able to see the dashboard
         WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
@@ -66,76 +66,13 @@ public class TaskCreation {
 
     }
 
-    @Test
-    public void task_creation_testHelpDesk() {
-
-//As a HelpDesk user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.helpdesk"), ConfigurationReader.getProperty("password"));
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-//Navigate to the "Things to go" input box and pass the title of the task
-        WebElement taskTitle = driver.findElement(By.xpath("//input[@data-bx-id='task-edit-title']"));
-        taskTitle.sendKeys("This is the title");
-
-//Pass the task description into the empty task body below the task title
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        taskBody.sendKeys("These are the task details");
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “Task has been created” popup is displayed.
-        WebElement popup = driver.findElement(By.xpath("//div[@id='blogPostEditCreateTaskPopup_content']"));
-        Assert.assertTrue(popup.isDisplayed());
-
-    }
-
-    @Test
-    public void task_creation_testMarketing() {
-
-//As a Marketing user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.marketing"), ConfigurationReader.getProperty("password"));
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-//Navigate to the "Things to go" input box and pass the title of the task
-        WebElement taskTitle = driver.findElement(By.xpath("//input[@data-bx-id='task-edit-title']"));
-        taskTitle.sendKeys("This is the title");
-
-//Pass the task description into the empty task body below the task title
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        taskBody.sendKeys("These are the task details");
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “Task has been created” popup is displayed.
-        WebElement popup = driver.findElement(By.xpath("//div[@id='blogPostEditCreateTaskPopup_content']"));
-        Assert.assertTrue(popup.isDisplayed());
-
-    }
 
 
     @Test
-    public void taskWithoutTitle_creation_testHR() {
+    public void taskWithoutTitle_creation_test() {
 
 //As an HR user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.hr"), ConfigurationReader.getProperty("password"));
+        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
 //I'm able to see the dashboard
         WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
         Assert.assertTrue(taskTab.isDisplayed());
@@ -157,65 +94,14 @@ public class TaskCreation {
         Assert.assertTrue(message.isDisplayed());
     }
 
-    @Test
-    public void taskWithoutTitle_creation_testMarketing() {
 
-//As a Marketing user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.marketing"), ConfigurationReader.getProperty("password"));
-
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “The task name is not specified.” massage is displayed.
-        WebElement message = driver.findElement(By.xpath("//div[.='The task name is not specified.']"));
-        Assert.assertTrue(message.isDisplayed());
-    }
-
-    @Test
-    public void taskWithoutTitle_creation_testHelpDesk() {
-
-//As a HelpDesk user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.helpdesk"), ConfigurationReader.getProperty("password"));
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “The task name is not specified.” massage is displayed.
-        WebElement message = driver.findElement(By.xpath("//div[.='The task name is not specified.']"));
-        Assert.assertTrue(message.isDisplayed());
-
-
-    }
 
 
     @Test
-    public void taskWithoutTitle_creation_testHR_space() {
+    public void taskWithoutTitle_creation_space_test() {
 
 //As an HR user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.hr"), ConfigurationReader.getProperty("password"));
+        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username"), ConfigurationReader.getProperty("password"));
 //I'm able to see the dashboard
         WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
         Assert.assertTrue(taskTab.isDisplayed());
@@ -239,60 +125,6 @@ public class TaskCreation {
         Assert.assertTrue(message.isDisplayed());
     }
 
-    @Test
-    public void taskWithoutTitle_creation_testMarketing_space() {
 
-//As a Marketing user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.marketing"), ConfigurationReader.getProperty("password"));
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-// Pass space into "Things to do" input box.
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        taskBody.sendKeys(" " + Keys.ENTER);
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “The task name is not specified.” massage is displayed.
-        WebElement message = driver.findElement(By.xpath("//div[.='The task name is not specified.']"));
-        Assert.assertTrue(message.isDisplayed());
-    }
-
-    @Test
-    public void taskWithoutTitle_creation_testHelpDesk_space() {
-
-//As a HelpDesk user go to  the main page of the website
-        CRM_Utilities.crm_login(driver, ConfigurationReader.getProperty("username.helpdesk"), ConfigurationReader.getProperty("password"));
-
-//I'm able to see the dashboard
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
-        Assert.assertTrue(taskTab.isDisplayed());
-
-//Click to the 'TASK' tab in the upper part of the webpage
-        taskTab.click();
-
-// Pass space into "Things to do" input box.
-        driver.switchTo().frame(driver.findElement(By.xpath("//div[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement taskBody = driver.findElement(By.xpath("//html/body[@style='min-height: 84px;']"));
-        taskBody.sendKeys(" " + Keys.ENTER);
-        driver.switchTo().parentFrame();
-
-//Click 'SEND' button
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id='blog-submit-button-save']"));
-        sendBtn.click();
-
-//Verify “The task name is not specified.” massage is displayed.
-        WebElement message = driver.findElement(By.xpath("//div[.='The task name is not specified.']"));
-        Assert.assertTrue(message.isDisplayed());
-
-    }
 
 }
